880309aabe10096a7975669313e23235c5b03885
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2023-09-10 15:21:13 -0400

    cleaned up the login and unsued code

--- makingFitnessBetterTypescript/mfbreact/src/components/layout/HomepageComponent.tsx
+++ makingFitnessBetterTypescript/mfbreact/src/components/layout/HomepageComponent.tsx
@@ -21,48 +21,7 @@ const HomepageComponent = () => {
 
     function NavigateToRegistrationForm(){
         navigate("/registration");
-
     }
-
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    // const [remember, setRemember] = useState(false);
-    const [error,setError]=useState(false);
-    const [message,setMessage]=useState("")
-
-    function loginUser(e:any){
-        e.preventDefault();
-        auth.login(username,password).then((response)=>{
-            console.log(response.data);
-
-            if(response.data.token){
-                localStorage.setItem("token",response.data.token);
-                localStorage.setItem("email", response.data.email);
-                localStorage.setItem("role",response.data.role);
-                authContext.setToken(response.data.token);
-            }
-
-            const currentUser = auth.getUser(username).then((response) => {
-                if(response.data){
-                    localStorage.setItem("id",response.data.memberId);
-                    localStorage.setItem("username",response.data.username);
-                }
-            });
-
-            navigate("/");
-
-            handleClose();
-
-        }).catch(err=>{
-            setMessage(err.response.data.exception);
-            setError(true)
-        });
-        // localStorage.setItem("remember-me",String(remember));
-    }
-
 
 
     // @ts-ignore
@@ -70,29 +29,6 @@ const HomepageComponent = () => {
         <>
 
             <MainNavigation/>
-            <div>
-                <Button variant="primary" onClick={handleShow}>
-                    Login
-                </Button>
-            </div>
-
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Login Form</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Login onSubmit={loginUser} username={username} setUsername={setUsername} password={password}
-                           setPassword={setPassword} />
-
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close Modal
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-
-
 
             <Container className = 'page-container'>
 
